0f8bd920a552de84c5d726634ba6c4a3de6437a9
dilsy233323346 <119727151+dilsy233323346@users.noreply.github.com>
2023-05-08 21:37:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['447710173736', 'Owner', true],
-  ['447710173736', 'Ash', true], 
+  ['17866775192', 'Owner', true],
+  ['17866775192', 'Ahmad raza', true], 
   [''] 
 ] //Number of owners
 
